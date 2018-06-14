aaf0f3570bd560f9c2bb6fcaa9f6358674c91fce
Kiklik <autchariya.c@kkumail.com>
2018-06-14 16:48:21 +0700

    makeup

---
+++ app/src/main/java/com/autchariya/makeuptutorial/MainActivityMode.java
@@ -0,0 +1,20 @@
+package com.autchariya.makeuptutorial;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivityMode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_mode);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        return super.onCreateOptionsMenu(menu);
+    }
+}
